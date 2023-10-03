aa43af4a9eaa97ac9b1699cf81d0837bf3cb756e
xentrix <filip.kosmel@maurit.sk>
2023-10-03 16:17:34 +0200

    parameters

---
+++ src/Service/MixRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Cache\CacheItemInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MixRepository
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private CacheInterface $cache,
+        private bool $isDebug
+    )
+    {
+
+    }
+    public function findAll(): array
+    {
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+
+            return $response->toArray();
+        });
+    }
+}
