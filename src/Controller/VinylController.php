c4bb477ba6c35ef7ba77178eb4e40965075e8fa1
xentrix <filip.kosmel@maurit.sk>
2023-10-04 10:14:44 +0200

    adding dev secrets vault

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -10,6 +10,13 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private MixRepository $mixRepository,
+        private bool $isDebug,
+    )
+    {
+    }
+
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -29,10 +36,10 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(MixRepository $mixRepository, string $slug = null): Response
+    public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
+        $mixes = $this->mixRepository->findAll();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -10,19 +13,25 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
         #[Autowire('$kernel.debug%')]
-        private bool $isDebug
+        private bool $isDebug,
+//        #[Autowire(service: 'twig.command.debug')]
+//        private DebugCommand $twigDebugCommand,
     )
     {
 
     }
     public function findAll(): array
     {
+//        $output = new BufferedOutput();
+//        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+//        dd($output);
+
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
         });
