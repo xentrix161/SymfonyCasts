285e9e6bd1fe923933d6e60a082d5dbf8e3a18ab
xentrix <filip.kosmel@maurit.sk>
2023-10-04 11:30:25 +0200

    command test

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\MixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -12,11 +13,11 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:talk-to-me',
-    description: 'Add a short description for your command',
+    description: 'A self-aware command that can do... only one thing.',
 )]
 class TalkToMeCommand extends Command
 {
-    public function __construct()
+    public function __construct(private MixRepository $mixRepository)
     {
         parent::__construct();
     }
@@ -24,25 +25,30 @@ class TalkToMeCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Tvoje meno')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Shall I yell?')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $name = $input->getArgument('name') ?: 'whoever you are';
+        $yell = $input->getOption('yell');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $msg = sprintf('Hey %s!', $name);
+        if ($yell) {
+            $msg = strtoupper($msg);
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $io->success($msg);
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        if ($io->confirm('Do you want a mix recommendation?')) {
+            $mixes = $this->mixRepository->findAll();
+            $mix = $mixes[array_rand($mixes)];
+
+            $io->note('I recommend the mix: ' . $mix['title']);
+        }
 
         return Command::SUCCESS;
     }
